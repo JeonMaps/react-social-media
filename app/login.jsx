27f43b32fe147c01136150a0a960f2421e1af224
Jeon Maps <65327532+JeonMaps@users.noreply.github.com>
2024-10-08 17:39:56 +0800

    login and signup UI

--- app/login.jsx
+++ app/login.jsx
@@ -1,20 +1,77 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
-import Button from '../components/Button'
-import { wp } from '../helpers/common'
-import  Home from '../assets/icons/Home'
+import { hp, wp } from '../helpers/common'
 import { useRouter } from 'expo-router'
 import { theme } from '../constants/theme'
+import Icon from '../assets/icons'
+import { StatusBar } from 'expo-status-bar'
+import BackButton from '../components/BackButton'
+import Input from '../components/Input'
+import Button from '../components/Button'
+
 
 const Login = () => {
   const router = useRouter();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async () => {
+    if(!emailRef.current || !passwordRef.current) {
+      Alert.alert("Login", "Please fill out all the fields!")
+      return;
+    }
+  }
   return (
     <ScreenWrapper>
-      <Text>Login</Text>
-      <TextInput></TextInput>
-      <Home strokeWidth={2} color={theme.colors.rose} onPress={() => {router.push('welcome')}}></Home>
-      <Button title="Login" buttonStyle={{marginHorizontal: wp(20)}}/>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <BackButton router={router} />
+
+        {/* welcome */}
+        <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back!</Text>
+        </View>
+
+        {/* form */}
+        <View style={styles.form}>
+          <Text styles={{ fontSize: hp(1.5), color: theme.colors.text }}>
+            Please login to continue
+          </Text>
+          <Input
+            icon={<Icon name="mail" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your email"
+            onChangeText={value => emailRef.current = value}
+          />
+          <Input
+            icon={<Icon name="lock" size={26} strokeWidth={1.6} />}
+            placeholder="Enter your password"
+            secureTextEntry
+            onChangeText={value => passwordRef.current = value}
+          />
+          <Text style={styles.forgotPassword}>
+            Forgot Password?
+          </Text>
+        </View>
+
+        {/* button */}
+        <Button title={"Login"} loading={loading} onPress={onSubmit} />
+
+        {/* footer */}
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            Don't have an account?
+          </Text>
+          <Pressable onPress={() => router.push("signUp")}>
+            <Text style={[styles.footerText, { color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold }]}>
+              Sign Up
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </ScreenWrapper>
   )
 }
@@ -22,5 +79,33 @@ const Login = () => {
 export default Login
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5)
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text
+  },
+  form: {
+    gap: 25
+  },
+  forgotPassword: {
+    textAlign: 'right',
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+  footerText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6)
+  }
 })
