b82bc260b4bf871a7e6ceab9f577ca190a7266d4
Jeon Maps <65327532+JeonMaps@users.noreply.github.com>
2024-10-21 21:46:18 +0800

    Update postService.js

--- services/postService.js
+++ services/postService.js
@@ -2,31 +2,31 @@ import { supabase } from "../lib/supabase";
 import { uploadFile } from "./imageService";
 
 export const createOrUpdatePost = async (post) => {
-    try{
+    try {
         //upload image
-        if(post.file && typeof post.file=='object'){
+        if (post.file && typeof post.file == 'object') {
             let isImage = post?.file?.type == 'image'
             let folderName = isImage ? 'postImages' : 'postVideos'
             let fileResult = await uploadFile(folderName, post?.file?.uri, isImage);
-            if(fileResult.success) post.file = fileResult.data
-            else{
+            if (fileResult.success) post.file = fileResult.data
+            else {
                 return fileResult
             }
         }
 
-        const {data, error } = await supabase
-        .from('posts')
-        .upsert(post)
-        .select()
-        .single()
+        const { data, error } = await supabase
+            .from('posts')
+            .upsert(post)
+            .select()
+            .single()
 
-        if(error){
+        if (error) {
             console.log('createPost error: ', error)
-            return {success:false, msg: 'Could not create your post'}
+            return { success: false, msg: 'Could not create your post' }
         }
-        return {success:true, data: data}
-    } catch(error){
+        return { success: true, data: data }
+    } catch (error) {
         console.log('createPost error: ', error)
-        return {success:false, msg: 'Could not create your post'}
+        return { success: false, msg: 'Could not create your post' }
     }
 }
